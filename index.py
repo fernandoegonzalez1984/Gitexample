ca0eb0fe21ebff284b3bf33294c9dc652a649825
Fer Gonzalez <f.gonzalez@awtestlab.com>
2022-11-11 14:00:05 -0500

    Second update to index.py

--- index.py
+++ index.py
@@ -3,10 +3,10 @@ name = input ("What is your name ? ")
 
 if name == "Diego Armando Maradona":
     print("D10S")
-elif name == "Pele":
-    print("Debuto con un pibe")
-elif name == "Pasman":
-    print("Pasman vos tambien la tenes adentro")
+elif name == "Pipi Novello":
+    print("Guason")
+elif name == "Lope":
+    print("Flauta Manager")
 
 else:
     print("Hello " + name + " ,nice to see you!")
